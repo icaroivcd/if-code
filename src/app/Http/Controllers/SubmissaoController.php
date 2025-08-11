4ede7a128a268a4c1bc03f0553bb9ff1c29498dc
Maicon Leles <mpml@aluno.ifnmg.edu.br>
2025-08-11 17:26:43 -0300

    feat(submissao): cria envio e visualizacao de submissao
    Implementa os métodos e rotas para permitir a criação de submissões e visualização dos resultados dessas submissões.

--- src/app/Http/Controllers/SubmissaoController.php
+++ src/app/Http/Controllers/SubmissaoController.php
@@ -2,9 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Submissao;
+use App\Services\SubmissaoService;
+use Exception;
 use Illuminate\Http\Request;
+use Throwable;
 
 class SubmissaoController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $submissoes = Submissao::all();
+
+        return response()->json($submissoes);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @throws Throwable
+     */
+    public function store(Request $request)
+    {
+        $submissaoService = new SubmissaoService($request);
+
+        if (!$submissaoService->salvar()) {
+            return response()->json(['message' => 'Erro ao salvar submissao'], 500);
+        }
+
+        return response()->json([
+            'message' => 'Submissão criada com sucesso!',
+            'submissao' => $submissaoService->getSubmissao()->only(['id'])
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Submissao $submissao)
+    {
+        try {
+            $status = $submissao->getStatus();
+        } catch (Exception $e) {
+            return response()->json($e->getMessage(), 500);
+        }
+
+        return response()->json($status);
+    }
+
+//    /**
+//     * Update the specified resource in storage.
+//     */
+//    public function update(Request $request, Problema $submissao)
+//    {
+//
+//    }
+//    /**
+//     * Remove the specified resource from storage.
+//     */
+//    public function destroy(Submissao $submissao)
+//    {
+//        try{
+//            $submissao->delete();
+//        } catch(Exception $e){
+//            return response()->json(['Erro ao apagar.'], 400);
+//        }
+//
+//        return response()->json(['Apagado com sucesso!']);
+//    }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AtividadeController;
 use App\Http\Controllers\ProblemaController;
+use App\Http\Controllers\SubmissaoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 Route::apiResource('atividade', AtividadeController::class);
 Route::apiResource('problema', ProblemaController::class);
+Route::apiResource('submissao', SubmissaoController::class)
+->except('update', 'destroy');
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
