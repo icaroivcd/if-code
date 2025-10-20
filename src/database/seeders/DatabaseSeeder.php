1b67dfe89aa6fb24f621fcd298fd7593a2b35b50
Marcos Marques <mdmarques70@gmail.com>
2025-10-20 16:00:00 -0300

    refactor: modularize role and user seeders

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -4,8 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
-use Spatie\Permission\Models\Role;
+use Database\Seeders\RBAC\RoleSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,16 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        $admin = \App\Models\User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@admin.com',
-            'password' => Hash::make('12345678'),
+        $this->call([
+            RoleSeeder::class,
+            UserSeeder::class,
         ]);
-
-        Role::create(['name' => 'admin']);
-
-        $admin->assignRole('admin');
     }
 }
