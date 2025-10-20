f8b0c90938c9121506ec0a338d6db1bbd92c49aa
Marcos Marques <mdmarques70@gmail.com>
2025-10-20 15:35:03 -0300

    create: model Professor

---
+++ src/app/Models/Professor.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Professor extends Model
+{
+    use HasFactory;
+
+    protected $table = "professor";
+
+
+    protected $fillable = [
+        'area_atuacao',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id');
+    }
+
+    // public function turmas()
+    // {
+    //     return $this->hasMany(Turma::class, 'professor_id');
+    // }
+
+    public function problemas()
+    {
+        return $this->hasMany(Problema::class, 'professor_id');
+    }
+}
