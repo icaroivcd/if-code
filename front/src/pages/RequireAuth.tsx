3ac3c7ef57405ed5d6dfa34767c576292a30e25c
felkng <felipersf374@gmail.com>
2025-10-24 12:26:06 -0300

    Fix redirecting to login component

--- front/src/pages/RequireAuth.tsx
+++ front/src/pages/RequireAuth.tsx
@@ -1,27 +1,11 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import Loading from "@/components/Loading";
+import { useUser } from "@/context/UserContext";
 import { Navigate, Outlet } from "react-router";
 
 export default function RequireAuth() {
-    const [loading, setLoading] = useState(true);
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const { user, loading } = useUser();
 
-    useEffect(() => {
-      axios.get("/api/user", { withCredentials: true })
-        .then(res => {
-          if (res.data && res.data.id) {
-            setIsAuthenticated(true);
-          } else {
-            setIsAuthenticated(false);
-          }
-        })
-        .catch(() => {
-          setIsAuthenticated(false);
-        })
-        .finally(() => setLoading(false));
-    }, []);
+    if (loading) return <Loading />;
 
-    if (loading) return null;
-
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+    return user ? <Outlet /> : <Navigate to="/login" replace />;
 }
