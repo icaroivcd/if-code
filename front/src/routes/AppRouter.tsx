2f0bb8489f24f48a269ed895334dc86d7bd5ddbe
felkng <felipersf374@gmail.com>
2025-10-24 12:26:57 -0300

    Use redirecting to login page

--- front/src/routes/AppRouter.tsx
+++ front/src/routes/AppRouter.tsx
@@ -3,6 +3,8 @@ import Activities from "@/pages/activities/Activities";
 import ActivitiesDetails from "@/pages/activitiesDetails/ActivitiesDetails";
 import AppLayout from "@/pages/AppLayout";
 import Home from "@/pages/home/Home";
+import Login from "@/pages/login/Login";
+import RequireAuth from "@/pages/RequireAuth";
 import Submissions from "@/pages/submissions/Submissions";
 import SubmissionsDetails from "@/pages/submissionsDetails/SubmissionsDetails";
 import { BrowserRouter, Route, Routes } from "react-router";
@@ -11,19 +13,22 @@ export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<App />} />
-          <Route path="home" element={<Home />} />
-          <Route path="activities">
-            <Route index element={<Activities />} />
-            <Route path=":id" element={<ActivitiesDetails />} />
-          </Route>
-          <Route path="submissions">
-            <Route index element={<Submissions />} />
-            <Route
-              path=":activityId/:submissionId"
-              element={<SubmissionsDetails />}
-            />
+        <Route path="/login" element={<Login />} />
+        <Route element={<RequireAuth />}>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<App />} />
+            <Route path="home" element={<Home />} />
+            <Route path="activities">
+              <Route index element={<Activities />} />
+              <Route path=":id" element={<ActivitiesDetails />} />
+            </Route>
+            <Route path="submissions">
+              <Route index element={<Submissions />} />
+              <Route
+                path=":activityId/:submissionId"
+                element={<SubmissionsDetails />}
+              />
+            </Route>
           </Route>
         </Route>
       </Routes>
